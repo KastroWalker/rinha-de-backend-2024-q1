054414e622073e1259f5272614c75c28c0281e46
Victor Castro <victorcsa2002@gmail.com>
2024-02-18 22:05:52 -0300

    feat: get database url from enviroment variabel

--- src/main/kotlin/kastro/dev/repositories/Database.kt
+++ src/main/kotlin/kastro/dev/repositories/Database.kt
@@ -5,6 +5,5 @@ import java.sql.DriverManager
 
 object Database {
     // TODO : Get data from env
-    private const val JDBC_URL = "jdbc:postgresql://localhost:5433/transactiondb"
-    val connection: Connection = DriverManager.getConnection(JDBC_URL, "postgres", "postgres")
+    val connection: Connection = DriverManager.getConnection(System.getenv("DATABASE_URL"), "postgres", "postgres")
 }
